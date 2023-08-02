59074974df3786f841d6de25552aed4ef41a8676
Sibtain Raza <sibtainraza949@gmail.com>
2023-08-02 14:49:25 +0530

    create notes  and header components

---
+++ src/components/Notes.tsx
@@ -0,0 +1,15 @@
+function Notes() {
+  return (
+    <div className="grid-item">
+      <p>
+        Elevate your process writing to new heights with the assistance of our
+        AI Text Generator. Scribe AI offers grammar and style suggestions,
+        helping you refine your work and ensure a polished final product. Let AI
+        be your writing partner, making your words shine. 3. Build & share
+        documentation — fast Cut your process documentation .
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
