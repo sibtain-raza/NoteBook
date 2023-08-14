5702d8e0372649df54b5490977da77d0b40da855
Sibtain Raza <sibtainraza949@gmail.com>
2023-08-14 16:31:58 +0530

    add a home button on nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,6 @@
 import "./NavBar.css";
+import { Link } from "react-router-dom";
+import HomeImage from "../assets/svg/icons8-home.svg";
 import bookImage from "../assets/svg/Logo.svg";
 
 function NavBar() {
@@ -6,6 +8,9 @@ function NavBar() {
     <div className="header">
       <img src={bookImage} />
       <h2>NOTEBOOK</h2>
+      <Link to={"/"} className="home-button">
+        <img src={HomeImage} />
+      </Link>
     </div>
   );
 }
